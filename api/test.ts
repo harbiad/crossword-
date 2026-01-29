bcbac8ae6949bb9550627ca7b0e82a7b2af0f4c5
Ahmed Alharbi <harbixyz@hotmail.com>
2026-01-30 01:45:31 +0300

    Test dict import

--- api/test.ts
+++ api/test.ts
@@ -1,9 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { DICT_A1_A2 } from './_dict_cefr_en_ar';
 
 export const config = {
   runtime: 'nodejs',
 };
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ ok: true, method: req.method });
+  res.status(200).json({ ok: true, dictSize: Object.keys(DICT_A1_A2).length });
 }
