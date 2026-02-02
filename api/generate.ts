cfaa0dc372c04d90df02c09e2761877a35a6cc8b
Ahmed Alharbi <harbixyz@hotmail.com>
2026-02-02 21:16:01 +0300

    Fix ESM import extension and bump header

--- api/generate.ts
+++ api/generate.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { DICT_COMMON_3000 } from './dict_common_3000';
+import { DICT_COMMON_3000 } from './dict_common_3000.js';
 
 export const config = {
   runtime: 'nodejs',
